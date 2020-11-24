3ca5b2cc32bbbee5ff5b54ab1fd0a4a5ea144799
Ryan Byrne <c17326283@mytudublin.ie>
2020-11-24 23:20:13 +0000

    Simple 1x1 mesh generation

---
+++ GE1-WorldGenerator/Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]//Make sure the scripts this relies on are on obj
+public class MeshGenerator : MonoBehaviour
+{
+    private Mesh mesh;
+
+    private Vector3[] vertices;
+
+    private int[] triangles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;//set to just created mesh
+        
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(0, 0, 1),
+            new Vector3(1, 0, 0),
+            new Vector3(1, 0, 1) 
+        };
+
+        triangles = new int[]
+        {
+            0, 1, 2,
+            1, 3, 2//make sure points are inputted in clockwise way
+        };
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        
+        mesh.RecalculateNormals();//fix the weird normals that usually come with vertice creation
+    }
+}
