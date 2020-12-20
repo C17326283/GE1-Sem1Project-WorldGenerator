4b6e06af852b9f7850656c486d9eaf5affaac8e4
Ryan Byrne <c17326283@mytudublin.ie>
2020-12-20 00:14:07 +0000

    Generating cube mesh

--- GE1-WorldGenerator/Assets/Scripts/Planet.cs
+++ GE1-WorldGenerator/Assets/Scripts/Planet.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Range(2, 256)] 
+    public int res = 10;
+    
+    [SerializeField, HideInInspector]
+    private MeshFilter[] meshFilters;
+    private TerrainFace[] terrainFaces;
+
+
+    private void OnValidate()
     {
-        
+        Initialize();
+        GenerateMesh();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Initialize()
     {
-        
+        if (meshFilters == null || meshFilters.Length == 0)
+        {
+            meshFilters = new MeshFilter[6];
+        }
+        terrainFaces = new TerrainFace[6];
+
+        Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back};
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+            
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                meshFilters[i].sharedMesh = new Mesh();
+            
+                terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh,res,directions[i]);
+            }
+        }
+    }
+
+    void GenerateMesh()
+    {
+        foreach (TerrainFace face in terrainFaces)
+        {
+            face.ConstructMesh();
+        }
     }
 }
--- GE1-WorldGenerator/Assets/Scripts/TerrainFace.cs
+++ GE1-WorldGenerator/Assets/Scripts/TerrainFace.cs
@@ -2,17 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TerrainFace : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Mesh mesh;
+
+    private int res;
+
+    private Vector3 localUp;
+    private Vector3 axisA;
+    private Vector3 axisB;
+
+
+    public TerrainFace(Mesh mesh, int res, Vector3 localUp)
     {
+        this.mesh = mesh;
+        this.res = res;
+        this.localUp = localUp;
         
+        axisA = new Vector3(localUp.y, localUp.z,localUp.x);
+        axisB = Vector3.Cross(localUp, axisA);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ConstructMesh()
     {
+        Vector3[] vertices = new Vector3[res*res];
+        int[] triangles = new int[(res-1)*(res-1)*6];
+        int triIndex = 0;
+
+        for (int y = 0; y < res; y++)
+        {
+            for (int x = 0; x < res; x++)
+            {
+                int i = x + y * res;
+                Vector2 percent = new Vector2(x,y) / (res-1);
+                Vector3 pointOnUnitCube = localUp + (percent.x - .5f)*2*axisA + (percent.y - .5f)*2*axisB;
+                vertices[i] = pointOnUnitCube;
+                
+                if(x != res-1 &&  y != res-1)
+                {
+                    triangles[triIndex] = i;
+                    triangles[triIndex+1] = i+res+1;
+                    triangles[triIndex+2] = i+res;
+                    
+                    triangles[triIndex+3] = i;
+                    triangles[triIndex+4] = i+1;
+                    triangles[triIndex+5] = i+res+1;
+
+                    triIndex += 6;
+                }
+            }
+        }
         
+        mesh.Clear();//clear data from mesh
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
     }
 }
