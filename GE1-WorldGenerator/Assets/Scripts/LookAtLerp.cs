d489879cb5231ee662b1ef944c6accd327cc7323
Ryan Byrne <c17326283@mytudublin.ie>
2020-12-22 12:32:31 +0000

    BetterAtmosphere lighting

---
+++ GE1-WorldGenerator/Assets/Scripts/LookAtLerp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtLerp : MonoBehaviour
+{
+    public GameObject gameObjectToLookAt;
+    public float lerpSpeed = 2f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 relativePos = gameObjectToLookAt.transform.position - transform.position;
+        Quaternion toRotation = Quaternion.LookRotation(relativePos);
+        transform.rotation = Quaternion.Lerp( transform.rotation, toRotation, lerpSpeed * Time.deltaTime );
+    }
+}
