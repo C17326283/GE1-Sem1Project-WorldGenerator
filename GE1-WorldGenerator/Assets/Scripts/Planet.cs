998473ad1c71f2ccd177addb15ccd2088307b1cc
Ryan Byrne <c17326283@mytudublin.ie>
2020-12-22 22:12:44 +0000

    Sectioned out but problem at edges

--- GE1-WorldGenerator/Assets/Scripts/Planet.cs
+++ GE1-WorldGenerator/Assets/Scripts/Planet.cs
@@ -44,35 +44,42 @@ public class Planet : MonoBehaviour
         //make the 6 sides of the cube that will be spherized
         if (meshFilters == null || meshFilters.Length == 0)
         {
-            meshFilters = new MeshFilter[6];
+            meshFilters = new MeshFilter[24];
         }
         terrainFaces = new TerrainFace[6];
         
         
         //get all the directions to be used as the sides of the cube
         Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back};
-    
+
+        int k = 0;
         //create objects and components for all the faces
         for (int i = 0; i < 6; i++)
         {
             UpdateSettings(planetSettings);
             //todo try to split face further
-            if (meshFilters[i] == null)
+            for (int j = 0; j < 4; j++)
             {
-                GameObject meshObj = new GameObject("mesh");
-                meshObj.transform.parent = transform;
-                meshObj.transform.position = transform.position;
+                print(k+""+j);
+                if (meshFilters[k+j] == null)
+                {
+                    GameObject meshObj = new GameObject("mesh");
+                    meshObj.transform.parent = transform;
+                    meshObj.transform.position = transform.position;
             
-                meshObj.AddComponent<MeshRenderer>();
-                meshFilters[i] = meshObj.AddComponent<MeshFilter>();
-                meshFilters[i].sharedMesh = new Mesh();
+                    meshObj.AddComponent<MeshRenderer>();
+                    meshFilters[k+j] = meshObj.AddComponent<MeshFilter>();
+                    meshFilters[k+j].sharedMesh = new Mesh();
+                }
+            
+                int randBiome = Random.Range(0, planetSettings.biomeGradients.Length);//todo make this based on amount of biomes
+
+                meshFilters[k+j].GetComponent<MeshRenderer>().sharedMaterial = planetSettings.planetMaterials[randBiome];
             }
             
-            int randBiome = Random.Range(0, planetSettings.biomeGradients.Length);//todo make this based on amount of biomes
-
-            meshFilters[i].GetComponent<MeshRenderer>().sharedMaterial = planetSettings.planetMaterials[randBiome];
             //add to list of faces
-            terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh,res,directions[i], randBiome,elevationMinMax, planetSettings);
+            terrainFaces[i] = new TerrainFace(meshFilters[k+0].sharedMesh,meshFilters[k+1].sharedMesh,meshFilters[k+2].sharedMesh, meshFilters[k+3].sharedMesh,res, directions[i],elevationMinMax, planetSettings);
+            k+=4;
         }
     }
 
--- GE1-WorldGenerator/Assets/Scripts/TerrainFace.cs
+++ GE1-WorldGenerator/Assets/Scripts/TerrainFace.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class TerrainFace : MonoBehaviour
 {
     //private ShapeGenerator shapeGenerator;
-    private Mesh mesh;
+    private Mesh mesh1;
+    private Mesh mesh2;
+    private Mesh mesh3;
+    private Mesh mesh4;
 
     private int res;//resolution of mesh
     public int biome;
@@ -16,18 +19,21 @@ public class TerrainFace : MonoBehaviour
     
     private PlanetSettings settings;
     private NoiseLayer[] noiseLayers;
+    private GameObject[] faceQuads;
     private TerrainMinMaxHeights elevationMinMax;
 
 
 
     //constructor for initalising the terrain face parameters
-    public TerrainFace(Mesh mesh, int res, Vector3 localUp, int biome, TerrainMinMaxHeights elevationMinMax,PlanetSettings planetSettings)
+    public TerrainFace(Mesh mesh1,Mesh mesh2,Mesh mesh3,Mesh mesh4, int res, Vector3 localUp, TerrainMinMaxHeights elevationMinMax,PlanetSettings planetSettings)
     {
         //this.shapeGenerator = shapeGenerator;
-        this.mesh = mesh;
+        this.mesh1 = mesh1;
+        this.mesh2 = mesh2;
+        this.mesh3 = mesh3;
+        this.mesh4 = mesh4;
         this.res = res;
         this.localUp = localUp;
-        this.biome = biome;
         this.elevationMinMax = elevationMinMax;
         this.settings = planetSettings;
         this.noiseLayers = planetSettings.noiseLayers;//copy all noise layers from planetSettings
@@ -43,15 +49,30 @@ public class TerrainFace : MonoBehaviour
         int r = (res/2)+1;
         
         //make an array for verticies and triangles based on the resolution
-        Vector3[] vertices = new Vector3[res*res];
+        Vector3[] allVertices = new Vector3[res*res];
+        //GameObject[] faceQuads = new GameObject[4];//intialize array
+        Vector3[] vertices1 = new Vector3[r*r];
         Vector3[] vertices2 = new Vector3[r*r];
-        int[] triangles = new int[(res-1)*(res-1)*6];//res-1^2 is num of faces * by 2 triangles per square * verticies per triangle
+        Vector3[] vertices3 = new Vector3[r*r];
+        Vector3[] vertices4 = new Vector3[r*r];
+        
+        
+        int[] allTriangles = new int[(res-1)*(res-1)*6];//res-1^2 is num of faces * by 2 triangles per square * verticies per triangle
+        int[] triangles1 = new int[(r - 1) * (r - 1) * 6];
         int[] triangles2 = new int[(r - 1) * (r - 1) * 6];
-        int triIndex = 0;//Index for each individual point
-        int triIndex2 = 0;//Index for each individual point
+        int[] triangles3 = new int[(r - 1) * (r - 1) * 6];
+        int[] triangles4 = new int[(r - 1) * (r - 1) * 6];
+        int allTriIndex = 0;//Index for each individual point
+        int triIndex1 = 0;
+        int triIndex2 = 0;
+        int triIndex3 = 0;
+        int triIndex4 = 0;
 
         int i = 0;
-        int k = 0;
+        int k1 = 0;
+        int k2 = 0;
+        int k3 = 0;
+        int k4 = 0;
         //edit each vertex to the right position on sphere
         for (int y = 0; y < res; y++)
         {
@@ -63,73 +84,101 @@ public class TerrainFace : MonoBehaviour
                 
                 
                 //use the spherized point with noise to find where it should be
-                vertices[i] = AddNoiseToVertex(pointOnUnitSphere);
+                allVertices[i] = AddNoiseToVertex(pointOnUnitSphere);
                 
                 //understood verticies using this vid at this time https://youtu.be/QN39W020LqU?t=439
                 //set the points to make triangles from vertexes//just based on way the points are indexed
                 if(x != res-1 &&  y != res-1)//as long as not on the right or bottom edge
                 {
                     //first triangle of square points. on a 4x4 grid this would be 0,5,4 because of clockwise allocation
-                    triangles[triIndex] = i;
-                    triangles[triIndex+1] = i+res+1;
-                    triangles[triIndex+2] = i+res;
+                    allTriangles[allTriIndex] = i;
+                    allTriangles[allTriIndex+1] = i+res+1;
+                    allTriangles[allTriIndex+2] = i+res;
                     
                     //second triangle of square points. 0,1,5
-                    triangles[triIndex+3] = i;
-                    triangles[triIndex+4] = i+1;
-                    triangles[triIndex+5] = i+res+1;
+                    allTriangles[allTriIndex+3] = i;
+                    allTriangles[allTriIndex+4] = i+1;
+                    allTriangles[allTriIndex+5] = i+res+1;
+                    
+                    allTriIndex += 6;
 
-                    if (x<r-1 && y<r-1)
+                    if (x<r-1 && y<r-1)//top left quad of shape
                     {
-                        vertices2[k] = vertices[i];
-                        Debug.Log(triIndex2+"    k"+k+"    triangles2.Length"+triangles2.Length+"     i"+i+"     x"+x+"     y"+y);
-                        triangles2[triIndex2] = k;//first vertex
-                        triangles2[triIndex2 + 1] = k + r + 1;//second vertex of the first triangle
-                        triangles2[triIndex2 + 2] = k + r;
+                        vertices1[k1] = allVertices[i];
+                        triangles1[triIndex1] = k1;//first vertex
+                        triangles1[triIndex1 + 1] = k1 + r + 1;//second vertex of the first triangle
+                        triangles1[triIndex1 + 2] = k1 + r;
 
-                        triangles2[triIndex2 + 3] = k;
-                        triangles2[triIndex2 + 4] = k + 1;
-                        triangles2[triIndex2 + 5] = k + r + 1;
+                        triangles1[triIndex1 + 3] = k1;
+                        triangles1[triIndex1 + 4] = k1 + 1;
+                        triangles1[triIndex1 + 5] = k1 + r + 1;
+                        triIndex1 += 6;
+                        k1++;
+                    }
+                    else if (x>r-1 && y<r-1)//top right
+                    {
+                        vertices2[k2] = allVertices[i];
+                        triangles2[triIndex2] = k2;//first vertex
+                        triangles2[triIndex2 + 1] = k2 + r + 1;//second vertex of the first triangle
+                        triangles2[triIndex2 + 2] = k2 + r;
+
+                        triangles2[triIndex2 + 3] = k2;
+                        triangles2[triIndex2 + 4] = k2 + 1;
+                        triangles2[triIndex2 + 5] = k2 + r + 1;
 
                         triIndex2 += 6;
-                        k++;
+                        k2++;
                     }
+                    else if (x<r-1 && y>r-1)//bottom left
+                    {
+                        vertices3[k3] = allVertices[i];
+                        triangles3[triIndex3] = k3;//first vertex
+                        triangles3[triIndex3 + 1] = k3 + r + 1;//second vertex of the first triangle
+                        triangles3[triIndex3 + 2] = k3 + r;
 
-                    triIndex += 6;
+                        triangles3[triIndex3 + 3] = k3;
+                        triangles3[triIndex3 + 4] = k3 + 1;
+                        triangles3[triIndex3 + 5] = k3 + r + 1;
+
+                        triIndex3 += 6;
+                        k3++;
+                    }
+                    else if (x>r-1 && y>r-1)//bottom right
+                    {
+                        vertices4[k4] = allVertices[i];
+                        triangles4[triIndex4] = k4;//first vertex
+                        triangles4[triIndex4 + 1] = k4 + r + 1;//second vertex of the first triangle
+                        triangles4[triIndex4 + 2] = k4 + r;
+
+                        triangles4[triIndex4 + 3] = k4;
+                        triangles4[triIndex4 + 4] = k4 + 1;
+                        triangles4[triIndex4 + 5] = k4 + r + 1;
+
+                        triIndex4 += 6;
+                        k4++;
+                    }
                 }
 
                 i++;
 
-                /*
-
-                if(x != meshHalf-1 &&  y != meshHalf-1 && i < meshHalf)
-                {
-                    Debug.Log(vertices2.Length+" "+i);
-                    vertices2[i] = vertices[i];
-                    
-
-                    if (x != meshHalf - 2 && y != meshHalf - 2) //as long as not on the right or bottom edge
-                    {
-                        Debug.Log(i);
-                        triangles2[triIndex] = i;
-                        triangles2[triIndex+1] = i+res+1;
-                        triangles2[triIndex+2] = i+res;
-                    
-                        triangles2[triIndex+3] = i;
-                        triangles2[triIndex+4] = i+1;
-                        triangles2[triIndex+5] = i+res+1;
-
-                    }
-                }
-                */
             }
         }
-        mesh.Clear();//clear data from mesh for when recalculating resolution
-        
-        mesh.vertices = vertices2;
-        mesh.triangles = triangles2;
-        
-        mesh.RecalculateNormals();
+        mesh1.Clear();//clear data from mesh for when recalculating resolution
+        mesh1.vertices = vertices1;
+        mesh1.triangles = triangles1;
+        mesh1.RecalculateNormals();
+        mesh2.Clear();//clear data from mesh for when recalculating resolution
+        mesh2.vertices = vertices2;
+        mesh2.triangles = triangles2;
+        mesh2.RecalculateNormals();
+        mesh3.Clear();//clear data from mesh for when recalculating resolution
+        mesh3.vertices = vertices3;
+        mesh3.triangles = triangles3;
+        mesh3.RecalculateNormals();
+        mesh4.Clear();//clear data from mesh for when recalculating resolution
+        mesh4.vertices = vertices4;
+        mesh4.triangles = triangles4;
+        mesh4.RecalculateNormals();
     }
     
     public Vector3 AddNoiseToVertex(Vector3 pointOnUnitSphere)
