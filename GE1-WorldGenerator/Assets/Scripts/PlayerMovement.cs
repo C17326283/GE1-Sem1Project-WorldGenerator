8133448436af3d28960f936503156b7dcf1d4d11
Ryan Byrne <c17326283@mytudublin.ie>
2020-12-24 20:49:49 +0000

    Added old character controller script but doesnt have gravity to the center of planet

--- GE1-WorldGenerator/Assets/Scripts/PlayerMovement.cs
+++ GE1-WorldGenerator/Assets/Scripts/PlayerMovement.cs
@@ -31,63 +31,53 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, planet.transform.position - gameObject.transform.position, out hit, 10000))
+        this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * rotSpeed * Time.deltaTime, 0);//get new rotation
+        if (characterController.isGrounded)
         {
-            Vector3.Lerp(transform.position, hit.transform.position, speed);
-        }
-        */
-        void Update()
-        {
-            this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * rotSpeed * Time.deltaTime, 0);//get new rotation
-            if (characterController.isGrounded)
-            {
-                moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
-                moveDirection = this.transform.TransformDirection(moveDirection) * moveSpeed;
+            moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = this.transform.TransformDirection(moveDirection) * moveSpeed;
 
-                if (Input.GetAxis("Vertical") != 0)//If input then move
+            if (Input.GetAxis("Vertical") != 0)//If input then move
+            {
+                Debug.Log("forward");
+                if (Input.GetKey(KeyCode.LeftShift))
                 {
-                    if (Input.GetKey(KeyCode.LeftShift))
-                    {
-                        maxSpeed = RunSpeed;
-                    }
-                    else
-                    {
-                        maxSpeed = walkSpeed;
-                    }
-                
-                    if(moveSpeed < maxSpeed)//get faster untill you reach desired speed
-                    {
-                        moveSpeed += 1.0f;//get faster
-                    }
-                    else if (moveSpeed > maxSpeed)
-                    {
-                        moveSpeed -= 1.0f;
-                    }
+                    maxSpeed = RunSpeed;
                 }
-                else//if stop pressing then slow down
+                else
                 {
-                    if(moveSpeed > 0.0f || moveSpeed > maxSpeed)//stop slowing at 0
-                    {
-                        moveSpeed = 0.0f;
-                    }
+                    maxSpeed = walkSpeed;
                 }
             
-                if (Input.GetButton("Jump"))
+                if(moveSpeed < maxSpeed)//get faster untill you reach desired speed
                 {
-                    moveDirection.y = jumpSpeed;
+                    moveSpeed += 1.0f;//get faster
+                }
+                else if (moveSpeed > maxSpeed)
+                {
+                    moveSpeed -= 1.0f;
                 }
             }
-            // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-            // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-            // as an acceleration (ms^-2)
-            moveDirection.y -= gravity * Time.deltaTime;
-
-            // Move the controller
-            characterController.Move(moveDirection * Time.deltaTime);
-            this.transform.Rotate(this.rotation);
-        }//End update
+            else//if stop pressing then slow down
+            {
+                if(moveSpeed > 0.0f || moveSpeed > maxSpeed)//stop slowing at 0
+                {
+                    moveSpeed = 0.0f;
+                }
+            }
         
-    }
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+        this.transform.Rotate(this.rotation);
+    }//End update
 }
